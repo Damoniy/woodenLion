ad3caeed3a553b1cfe04f717caa36ca4fc5ee289
sonOfGrace <emersonp199@gmail.com>
2023-02-11 22:32:53 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/woodenlion/WoodenLionApplication.kt
@@ -0,0 +1,11 @@
+package br.com.woodenlion
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WoodenLionApplication
+
+fun main(args: Array<String>) {
+	runApplication<WoodenLionApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/woodenlion/WordenLionApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.woodenlion
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WordenLionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
